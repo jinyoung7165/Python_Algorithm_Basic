7937a2f7cedb2ac9fe269beb3ea1d9324284c1bf
young <jinyoung7165@gmail.com>
2022-12-02 23:47:34 +0900

    22/12/02 - HashTable

--- 09LeetCode/HashTable/3.py
+++ 09LeetCode/HashTable/3.py
@@ -5,15 +5,14 @@
 #"pwwkew" => 3("wkew") !주의: pwkew는 Subsequence(부분수열)이다
 #슬라이딩 윈도우로 한 칸씩 이동하면서 윈도우 내에 문자 중복이 없도록 투 포인터로 윈도우 사이즈를 조정(bs왜 안써-> for문 한 번만 쭉 돌리며 start의 범위만 본다)
 def lengthOfLongestSubstring(s = "abcabcbb"):
-    used = {} #윈도우 내 이미 나온 문자들의 인덱스
-    max_length = start = 0
-    for index, char in enumerate(s):
-        #이미 전에 등장했던 문자면 "start" 갱신
-        if char in used and start <= used[char]:
-            start = used[char] + 1 #이전에 char이 나온 위치 다음부터 윈도우 시작
-        else: #최대 부분 문자열의 길이 갱신
-            max_length += max(max_length, index - start + 1)
-        
-        used[char] = index #사용한 문자의 위치 삽입
-        
-    return max_length
+    #원소 반복 없이 가장 긴 부분문자열
+    #현재 원소가 갱신된 시작 이후로 ~ 여태 지나쳐온 애들 중에 있는지 확인해야함
+    past = {}
+    max_len = 0
+    temp = 0 #시작 위치
+    for i, c in enumerate(s):
+        if c in past and past[c]>=temp: #이미 지나쳐 옴. 중복된 원소의 시작 위치 갱신
+            temp = past[c] + 1
+        max_len = max(max_len, i-temp+1)
+        past[c] = i
+    return max_len
---
+++ 09LeetCode/HashTable/36.py
@@ -0,0 +1,63 @@
+#Valid Sudoku. 9*9크기
+#각 행,열은 1-9 중복없이 가짐
+#3*3부분 맵은 1-9 중복없이 가짐
+#값이 존재하는 부분만 판단하면 됨
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        
+        # Given an arr of row/col/sub,
+        # check if there are duplicates.
+        def isValid(arr):
+            s = ''.join(arr).replace('.','')
+            return len(s) == len(set(s))
+        
+        # Check each row in the board
+        def checkRow():
+            for row in board:
+                if not isValid(row):
+                    return False
+            return True
+        
+        # Check each col in the board,
+        # To access each col, we first unpack the board into sperate lists using *
+        # We then zip these rows together into columns.
+        def checkCol():
+            for col in zip(*board):
+                if not isValid(col):
+                    return False
+            return True
+        
+        # To get each sub-box, we first get the top-left indices of each sub-box,
+        # We then go 3 steps on each row and 3 steps on each col to construct the box.
+        def checkSub():
+            for r in range(0,9,3):
+                for c in range(0,9,3):
+                    sub = [board[r+dr][c+dc] for dr in range(3) for dc in range(3)]
+                    if not isValid(sub):
+                        return False
+            return True
+        
+        # In order to be a valid Sudoku, all row, col, and sub-box need to be valid
+        return checkRow() and checkCol() and checkSub()
+'''
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        def isValid(arr):
+            s = ''.join(arr).replace('.','') #.제거, 모두 이어붙임
+            return len(s) == len(set(s)) #중복 없어야 함
+        
+        for row in board:
+            if not isValid(row):
+                return False
+        
+        for col in zip(*board):
+            if not isValid(col):
+                return False
+        
+        for i in range(0, 9, 3):
+            for j in range(0, 9, 3):
+                sub = [board[i+k][j+t] for k in range(3) for t in range(3)]
+                if not isValid(row):
+                    return False
+        return True
+'''
