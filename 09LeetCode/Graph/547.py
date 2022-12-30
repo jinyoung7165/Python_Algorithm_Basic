5f4f1cec985f2182887f7d3d6324d7dadd7ee1de
young <jinyoung7165@gmail.com>
2022-12-30 19:04:54 +0900

    22/12/30 - Review Graph

--- 09LeetCode/Graph/547.py
+++ 09LeetCode/Graph/547.py
@@ -1,15 +1,39 @@
 #Number of provinces
+#isConnected[i][j] = 1 if the ith city and the jth city are directly connected, and isConnected[i][j] = 0
+# province 개수
 class Solution:
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
-        processed = set()
-        cnt = 0
-        for r in range(len(M)): 
-            if r not in processed:
-                cnt += 1
-                stack = [i for i,v in enumerate(M[r]) if i != r and v == 1]
-                while stack:
-                    curr = stack.pop()
-                    if curr in processed: continue
-                    processed.add(curr)
-                    stack.extend([i for i,v in enumerate(M[curr]) if i != r and v == 1])
-        return cnt
+        visited = set() #양방향이므로 다시 방문x 위함
+        n = len(isConnected)
+        count = 0
+        for node in range(n):
+            if node not in visited:
+                count += 1 #하나의 province 방문
+                next_node_list = [i for i, v in enumerate(isConnected[node]) if i != node and v == 1] #현재노드와 연결된 노드
+
+                while next_node_list:
+                    next_node = next_node_list.pop()
+                    if next_node in visited: continue
+                    visited.add(next_node) #방문
+                    next_node_list.extend([i for i, v in enumerate(isConnected[next_node]) if i != node and v == 1]) #다음 연결된 곳을 추가해나감
+        return count
+    
+'''
+재귀 DFS - 아주 조금 느림
+'''
+class Solution(object):
+    def findCircleNum(self, M):
+        visited = [False for _ in range(len(M))]
+        count = 0
+        for i in range(len(M)):
+            if not visited[i]: #방문하지 않았으면 타고 내려가라
+                count += 1
+                self.dfs(M, i, visited)
+        return count
+                
+    def dfs(self, M, index, visited): #현재 노드랑 지나온 길 전달
+        visited[index] = True
+        for i in range(0, len(M)):
+            if i == index: continue #자신이면 pass
+            if not visited[i] and M[index][i] == 1: #방문하지 않았고 도달가능하면 타고 내려가라
+                self.dfs(M, i, visited)
--- 09LeetCode/Graph/851.py
+++ 09LeetCode/Graph/851.py
@@ -1,27 +1,51 @@
 #Loud and Rich
 #richer = [[a,b]] a>b 부자
 #quiet[i] i번째 사람의 quiet정도
-#output[i] i번째 사람 이상의 재력 가진 사람 중 가장 작은 quiet가지는 사람
+#output[i] i번째 사람 이상의 재력 가진 사람 중 "가장 작은 quiet"가지는 사람
 class Solution:
     def loudAndRich(self, richer: List[List[int]], quiet: List[int]) -> List[int]:
         n = len(quiet)
         graph = [[] for _ in range(n)]
-        result = [[] for _ in range(n)]
+        result = []
         for x, y in richer:
             graph[y].append(x) #y->x(더 rich한 쪽)
         
         def dfs(node): #현재 노드의 인덱스
-            if not graph[node]: return node #더 이상 없으면 자기자신
-            #min_quiet = node #자기자신
-            minimum = node
-            for neighbour in graph[node]:
-                candidate = dfs(neighbour)
-                if quiet[minimum] > quiet[candidate]:
-                    minimum = candidate 
-            return minimum  
+            if not graph[node]: return node #out간선 더 이상 없으면 자신이 가장 rich
             
+            min_quiet = node
+            quietness = quiet[node]
+            for next_node in graph[node]: #자기보다 rich한 사람
+                candidate = dfs(next_node)
+                if quietness > quiet[candidate]: #현재보다 quiet작으면 기록
+                    min_quiet = candidate
+                    quietness =  quiet[candidate]
+            return min_quiet #rich한 사람 중 가장 작은 rich
 
         for i in range(n):
             result.append(dfs(i))
         
+        return result
+    
+#시간 초과 해결  
+class Solution:
+    def loudAndRich(self, richer: List[List[int]], quiet: List[int]) -> List[int]:
+        n = len(quiet)
+        graph = [[] for _ in range(n)]
+        for x, y in richer:
+            graph[y].append(x) #y->x(더 rich한 쪽)
+        
+        result = [-1] * n
+        def dfs(node):
+            #least quiet person in this subtree
+            if result[node] == -1: #아직 기록되지 않음
+                result[node] = node #일단 자기자신. 현재 노드에 대한 min을 즉시 result[node]에 기록
+                for child in graph[node]:
+                    candidate = dfs(child)
+                    if quiet[candidate] < quiet[result[node]]:
+                        result[node] = candidate
+            return result[node]#이미 기록된 노드는 탐색하지 않고 바로 리턴(여러 노드가 하나의 노드를 가리킬 수 있기 때문)
+
+        for i in range(n):
+            result[i] = dfs(i)
         return result
