b486cb49b27af195bdcf877205c2838614225328
young <jinyoung7165@gmail.com>
2023-01-13 12:20:59 +0900

    23/01/13 - Graph, UnionFind

--- 09LeetCode/Graph/130.py
+++ 09LeetCode/Graph/130.py
@@ -1,37 +1,35 @@
 #Surrounded Regions
+#사방이 N으로 막혀있는 O영역을 N으로 바꿔라
+#border이 O이거나 그것과 연결된 곳은 바꿀 수 없음
 #BFS
 class Solution:
     def solve(self, board: List[List[str]]) -> None:
         """
         Do not return anything, modify board in-place instead.
         """
+        #border에 존재하거나, 그것과 붙어있는 O라면 바뀌면 안됨
         if not board or not board[0]:
             return
         R, C = len(board), len(board[0])
-        if R<=2 or C<=2:
+        if R<=2 or C<=2: #border만 존재
             return
         queue = collections.deque()
-        for r in range(R):
-            queue.append((r, 0))
-            queue.append((r, C - 1))
-
-        for c in range(len(board[0])):
-            queue.append((0, c))
-            queue.append((R - 1, c))
-
+        for i in range(R):
+            for j in range(C):
+                if i in [0, R-1] or j in [0, C-1] and board[i][j] == 'O': #border에 있는 O이면 넣어줌
+                    queue.append([i, j])
         while queue:
-            r, c = queue.popleft()
-            print(r, c)
-            if 0 <= r < R and 0 <= c < C and board[r][c] == 'O':
-                board[r][c] = 'N'
-                queue.append((r-1, c))
-                queue.append((r + 1, c))
-                queue.append((r, c + 1))
-                queue.append((r, c - 1))
+            x, y = queue.popleft()
+            if 0<=x<R and 0<=y<C and board[x][y] == 'O':
+                board[x][y] = '.' #border O거나 그것과 붙어있는 부분
+                queue.append([x, y-1])
+                queue.append([x, y+1])
+                queue.append([x+1, y])
+                queue.append([x-1, y])
 
-        for r in range(R):
-            for c in range(C):
-                if board[r][c] == 'N':
-                    board[r][c] = 'O'
-                else:
-                    board[r][c] = 'X'
+        for i in range(R):
+            for j in range(C):
+                if board[i][j] == '.':
+                    board[i][j] = 'O'
+                elif board[i][j] == 'O':
+                    board[i][j] = 'X'
--- 09LeetCode/Graph/684.py
+++ 09LeetCode/Graph/684.py
@@ -21,3 +21,44 @@ class Solution(object):
             graph[x].append(y)
             graph[y].append(x)
         return result.pop() if result else []
+#union find O(n)
+#무방향 그래프 내 사이클 판별에 서로소 집합 이용
+#각 간선 하나씩 확인, 두 노드의 루트 확인
+#루트노드가 서로 다르면 합집합 연산
+#루트노드가 같다면 사이클이 발생한 것
+# 특정 원소가 속한 집합을 찾기
+class Solution(object):
+    def findRedundantConnection(self, edges):
+        graph = collections.defaultdict(list)
+        result = []
+        v = len(edges)
+                    
+        parent = [0] * (v + 1) # 부모 테이블 초기화하기
+
+        # 부모 테이블상에서, 부모를 자기 자신으로 초기화
+        for i in range(1, v + 1):
+            parent[i] = i
+
+        # 특정 원소가 속한 집합의 루트 노드를 찾아나감
+        def find_parent(x):
+            # 루트 노드가 아니라면, 루트 노드를 찾을 때까지 재귀적으로 호출
+            if parent[x] != x:
+                parent[x] = find_parent(parent[x]) #부모의 부모를 찾음
+            return parent[x] # 루트 노드 리턴
+
+        # 두 원소가 속한 집합을 합치기(두 노드를 간선으로 이어줌)
+        def union_parent(a, b):
+            a = find_parent(a) #부모 노드의 루트 리턴
+            b = find_parent(b)
+            if a < b: # 번호가 작은 노드를 부모로 가리키게 함
+                parent[b] = a #b->a
+            else:
+                parent[a] = b #a->b
+
+        for a, b in edges:
+            # 사이클이 발생한 경우 종료
+            if find_parent(a) == find_parent(b): #부모가 같으면 이미 같은 집합에 있는 노드임
+                return [a, b]
+            # 사이클이 발생하지 않았다면 합집합(Union) 연산 수행
+            else:
+                union_parent(a, b)
