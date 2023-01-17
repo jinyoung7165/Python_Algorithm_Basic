3b7af1aea997bf8f0f47d29186380d4cfb42718d
young <jinyoung7165@gmail.com>
2023-01-17 20:14:09 +0900

    23/01/17 - Heapq

--- 09LeetCode/Graph/2497.py
+++ 09LeetCode/Graph/2497.py
@@ -3,28 +3,22 @@
 # = subset of edges of the given graph such that there exists a common node for all edges.
 class Solution:
     def maxStarSum(self, vals: List[int], edges: List[List[int]], k: int) -> int:
-        
-        m = defaultdict(list)                                                                 
-        
-        for x,y in edges:       
-            
-            if vals[y]>0:                    
-                
-                heapq.heappush(m[x], vals[y])   
-                if len(m[x])>k:                 
-                    heapq.heappop(m[x])         
-            
-            if vals[x]>0:
-                heapq.heappush(m[y], vals[x])  
-                if len(m[y])>k:
-                    heapq.heappop(m[y])
-            
-            
-        res = -math.inf
-        for i in range(len(vals)):           
-            tot = vals[i]                      
-            for nei_value in m[i]:              
-                tot+=nei_value                
-                
-            res = max(res, tot)                        
-        return res     
+        center_vals = defaultdict(list) #해당 center에 대한 결과
+
+        if k == 0: return max(vals)
+
+        for u, v in edges:
+            if vals[v] > 0: #중심으로부터 1단계만 보면 됨(양수면 플러스)
+                heapq.heappush(center_vals[u], vals[v])
+                if len(center_vals[u]) > k: #최대 k개의 간선만 포함 가능
+                    heapq.heappop(center_vals[u]) #제일 작은 거 뺌
+            if vals[u] > 0:
+                heapq.heappush(center_vals[v], vals[u])
+                if len(center_vals[v]) > k:
+                    heapq.heappop(center_vals[v])
+        max_result = -float('inf')
+        for i in range(len(vals)): #edges = [] 대비
+            center_vals[i].append(vals[i]) #노드 자신 더함
+            if sum(center_vals[i]) > max_result: 
+                max_result = sum(center_vals[i])
+        return max_result
